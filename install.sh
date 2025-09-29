17d8b60b5db2e5116ac97617a55e853b0ab7e6ff
etrademark <etrademark>
2025-09-29 21:13:38 +0200

    NOTE

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "This is the install script for $(gum style --foreground 212 'Low Effort Groups dotfiles').
+This script has no mercy to existing configuration, 
+if you have conflicting configuration, we kindly advise you to back it up, $(gum style --foreground 212 'as it will be destroyed'). 
+After all this has no effort put into it.
+These are the configuration files that might get overwritten, depending on options:"
